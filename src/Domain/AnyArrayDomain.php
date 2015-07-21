c123d6bc398189a25587b4f4e2b4fd771d9756f4
Kazuhiko TAMURA <tamura@tecnet.co.jp>
2015-07-21 14:02:56 +0900

    Forgotten to merge it

---
+++ src/Domain/AnyArrayDomain.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Omelet\Domain;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+use Omelet\Util\CaseSensor;
+
+class AnyArrayDomain extends DomainBase
+{
+    /**
+     * @var DomainBase
+     */
+    private $child;
+
+    public function __construct(DomainBase $child)
+    {
+        $this->child = $child;
+    }
+
+    protected function expandTypesInternal(array $availableParams, $name, $val, CaseSensor $sensor)
+    {
+        if (is_array($val) && count($val) === 0) {
+            return [];
+        }
+
+        return $this->expand($name, $val, $sensor,
+            function ($k, $v) use ($availableParams, $sensor) {
+                return $this->child->expandTypes($availableParams, $k, $v, $sensor, false);
+            }
+        );
+    }
+
+    protected function expandValuesInternal(array $availableParams, $name, $val, CaseSensor $sensor)
+    {
+        if (is_array($val) && count($val) === 0) {
+            return [];
+        }
+
+        return $this->expand($name, $val, $sensor,
+            function ($k, $v) use ($availableParams, $sensor) {
+                return $this->child->expandValues($availableParams, $k, $v, $sensor, false);
+            }
+        );
+    }
+
+    private function expand($name, $val, CaseSensor $sensor, callable $fn)
+    {
+        return array_reduce(
+            array_keys($val),
+            function (array &$tmp, $k) use ($name, $val, $sensor, $fn) {
+                $n = $name !== '' ? $sensor->convert($name, $k) : $k;
+
+                return $tmp + [$k => $fn($n, $val[$k])];
+            },
+            []
+        );
+    }
+
+    public function childDomain()
+    {
+        return $this->child;
+    }
+
+    protected function convertResultsInternal($results, AbstractPlatform $platform, CaseSensor $sensor)
+    {
+        if (! is_int(key($results))) {
+            // case outut parameter returning
+            return $this->convertResultBuiltin($results, $platform, $sensor);
+        }
+        
+        if (($this->child instanceof BuiltinDomain)) {
+            $selector = 
+                function ($v) use ($platform, $sensor) {
+                    return $this->convertResultBuiltin($v, $platform, $sensor);
+                };
+        }
+        else {
+            $selector = 
+                function ($v) use ($platform, $sensor) {
+                    return $this->child->convertResults($v, $platform, $sensor);
+                };
+        }
+            
+        return array_map($selector, $results);
+    }
+    
+    private function convertResultBuiltin($results, AbstractPlatform $platform, CaseSensor $sensor)
+    {
+        return array_reduce(
+            array_keys($results),
+            function (array &$tmp, $k) use ($results, $platform, $sensor) {
+                $n = $sensor->convert($k);
+                
+                return $tmp + [$n => $this->child->convertResults($results[$k], $platform, $sensor)];
+            },
+            []
+        );
+    }
+    
+    public static function __set_state($values)
+    {
+        return new AnyArrayDomain($values['child']);
+    }
+}
